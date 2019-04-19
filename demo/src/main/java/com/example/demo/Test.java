9d1206df0305296b36b9780c8dfa8692aeeca27f
chenhong <435101024@qq.com>
2019-04-19 15:22:38 +0800

    Revert "3"
    This reverts commit 86798884

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -5,7 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("dev-1");
         System.out.println("dev-2");
-        System.out.println("dev-3");
     }
 
 }
